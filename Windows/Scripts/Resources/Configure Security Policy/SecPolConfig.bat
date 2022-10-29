fc353a71d97b46962ab243656571a397388db920
Nawget <60802051+t3ggun@users.noreply.github.com>
2022-10-29 11:04:32 -0700

    Create SecPolConfig.bat

---
+++ Windows/Scripts/Resources/Configure Security Policy/SecPolConfig.bat
@@ -0,0 +1,15 @@
+@echo off
+
+::Check for Admin Rights
+net session > nul 2> nul
+if not %errorlevel% == 0 (
+	echo:&echo Error: Admin privileges required. Please run this script in administrator mode.
+	echo:&echo:Press any key to close the window.
+	pause > nul
+	exit
+)
+
+::Configure Security Policy From Template
+secedit /configure /db c:\windows\security\local.sdb /cfg "%~dp0Win10Secure.inf"
+
+pause
