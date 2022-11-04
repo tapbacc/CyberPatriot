4f66216c729391d055c3e1e33f20e3de0c7aa292
Nawget <alexanderdrhead@gmail.com>
2022-11-04 11:26:44 -0700

    Added user config and merged with secpol

---
+++ Windows/Scripts/Resources/Configure Security Policy And Users/ConfigSecPolUsers.ps1
@@ -0,0 +1,28 @@
+secedit /configure /db "$($env:WinDir)\security\local.sdb" /cfg "$($PSScriptRoot)\Win10Secure.inf" | Out-Null
+Write-Host "Applied all security policy settings"
+
+$users = Get-LocalUser | select -property name
+
+foreach ($user in $users) {
+    if ($user.Name -eq $env:UserName) {
+        continue
+    }
+    Set-LocalUser -Name $user.name -PasswordNeverExpires $false -UserMayChangePassword $true
+    net user $user.Name 'Cyb3rPatri0t!' | Out-Null
+}
+
+Write-Host "`nChanged password to Cyb3rPatri0t! and configured password settings for all users (other than you)"
+
+Write-Host "`nList of current users and groups:"
+$ignored = 'DefaultAccount', 'RenamedAdm', 'RenamedGue', 'WDAGUtilityAccount', $env:UserName
+foreach ($user in $users) {
+    if ($ignored -contains $user.Name) {
+        continue
+    }
+
+    Write-Host "`n$($user.Name)"
+    net user $user.Name | select-string 'Local Group Memberships'
+}
+    
+
+cmd /c pause
